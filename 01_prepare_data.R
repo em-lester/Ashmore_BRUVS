1c159abb4a0b3244daf0e4b5585d2283bcc37553
em-lester <emilykatelester@gmail.com>
2022-12-14 16:21:35 -1000

    metadata

--- 01_prepare_data.R
+++ 01_prepare_data.R
@@ -55,7 +55,7 @@ data <-read_csv(paste(study,"2004_Points.csv",sep="_"))%>%
  replace_na(list(family="Unknown",genus="Unknown",species="spp"))%>% # remove any NAs in taxa name
  #dplyr::mutate(genus=ifelse((family%in%c("Carangidae")&species%in%c("sp10")),"Pseudocaranx",genus)) %>%
  #dplyr::mutate(species=ifelse((species%in%c("sp10")),"spp",species)) %>%
-  dplyr::select(OP_CODE, Scientific, maxn, Location, Family, Genus, Species, TIMEFIRSTSEEN, TIMEMAXNUMSEEN, TIMEFIRSTFEED)%>%
+  dplyr::select(OP_CODE, Scientific, maxn, Family, Genus, Species, TIMEFIRSTSEEN, TIMEMAXNUMSEEN, TIMEFIRSTFEED)%>%
  dplyr::glimpse()
 
 
@@ -80,6 +80,8 @@ dat_2004_delay <- dat_2004_fed %>%
   dplyr::mutate(TimeFirstFed=as.numeric(TimeFirstFed))%>%
   dplyr::mutate(TimeFirstFed=as.numeric(TimeFirstFed))%>%
   mutate(DelayToFeed = TimeFirstFed-TimeFirstSeen)%>%
+  dplyr::select(OP_CODE, Year, TimeFirstFed, Family, Genus, Species, Scientific, TimeFirstSeen,
+                HasFed, DelayToFeed)%>%
   glimpse()
 
 ## Import 2016 time first seen data
@@ -127,26 +129,49 @@ dat_2016_delay <- dat_2016_fed %>%
   dplyr::mutate(TimeFirstFed=as.numeric(TimeFirstFed))%>%
   dplyr::mutate(TimeFirstFed=as.numeric(TimeFirstFed))%>%
   mutate(DelayToFeed = TimeFirstFed-TimeFirstSeen)%>%
+  dplyr::select(OP_CODE, Year, TimeFirstFed, Family, Genus, Species, Scientific, TimeFirstSeen,
+                HasFed, DelayToFeed)%>%
   glimpse()
 
+## Combine 2004 and 2016 datasets 
 
-dat_2016_fed <- dat_2016 %>%
-  mutate(HasFed = ifelse(TimeFirstFed > 0, "1", "0"))%>%
-  dplyr::mutate(HasFed=as.numeric(HasFed))%>%
-  dplyr::mutate(HasFed = replace_na(HasFed, 0)) %>%
+
+dat <- full_join(dat_2016_delay, dat_2004_delay)%>%
   glimpse()
 
+# Read in Opcode key
+dir()
+opcodes <- read.csv("Ashmore_BRUV_naming_key.csv")%>%
+  glimpse()
 
 # read in data from Zoe with metadata, MaxN and size class info
 
 metadat <- read.csv("Ashmorebehaviour_zoe_analysis.csv")%>%
   dplyr::select(Date, Video, Year, Depth, Coral, Habitat, Complexity, 
-                Treatment, Family, Genus_species, Genus, Species., Size_class., 
-                MaxN_species_.standardised_hour.)%>%
+              Treatment, Family, Genus_species, Genus, Species., Size_class., 
+              MaxN_species_.standardised_hour.)%>%
+  dplyr::rename(NAME.ANALYSIS.ZOE=Video)%>%
   dplyr::mutate(Year=as.numeric(Year))%>%
   glimpse()
 
-## To do: 
-# Add calculate Has fed, delay to feed
-# Add in metadata from Zoe
-# Add in size classes from Fishbase
+
+# Join datasets by OP_CODE.AIMS
+
+metadata_2004_2016 <- left_join(opcodes,metadat, by="NAME.ANALYSIS.ZOE")%>%
+  dplyr::rename(Species=Species.)%>%
+  dplyr::rename(OP_CODE=OP_CODE.AIMS)%>%
+  dplyr::rename(Size_class=Size_class.)%>%
+  dplyr::mutate(Scientific=paste(Family,Genus,Species,sep=" "))%>%
+  dplyr::select("OP_CODE", "Year", "Depth", "Coral", "Habitat", "Complexity", "Treatment")%>%
+  glimpse()
+
+
+
+## Now add metadata_2004_2016 to dat by OP_CODE and Scientific
+
+final_dat <- left_join(dat, metadata_2004_2016, by=c("OP_CODE", "Year"))%>%
+  glimpse()
+
+# To do: 
+#Add in size classes
+#figure out why last join has so many NAs... it will be something silly
